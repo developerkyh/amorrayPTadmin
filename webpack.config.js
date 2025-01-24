3df14ebf37d60996433cbf0f7c91d7cf97a9d409
amorray <developerkyh@gmail.com>
2025-01-24 16:17:51 +0900

    20250124

--- webpack.config.js
+++ webpack.config.js
@@ -1,166 +1,123 @@
 const path = require('path');
-const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const compiler = require('vue-template-compiler');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-let LiveReloadPlugin = require('webpack-livereload-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'production',
-  // mode: process.env.NODE_ENV || 'development',
-  devtool: 'source-map',
+  mode: 'development',
+  stats: {
+    children: true,
+  },
   devServer: {
-    contentBase: path.join(__dirname, '/dist/'),
-    hot: true,
-    publicPath: '/dist/',
-    compress: true,
+    static: {
+      directory: path.resolve(process.cwd(), "./dist/")
+    },
+    watchFiles: [
+      path.resolve(process.cwd(), "./index.html")
+    ],
+    // compress: true,
     port: 3000,
     historyApiFallback: {
       disableDotRule: true // Router를 사용할 경우 refresh 했을때 현페이지 유지
     },
-    // open:true,
-    headers: {
-      // "Access-Control-Allow-Origin": "localhost:22",
-      "Access-Control-Allow-Origin": "http://10.41.76.139",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    }
   },
   entry: {
-    app: [
-      '@babel/polyfill',
-      './build/js/index',
-      './build/src/main',
-      './build/less/AdminLTE',
-      './build/less/skins/_all-skins',
-    ]
+    // app: ['./src/index.js'],
+    // adminPT: ['./src/assets/styles/adminPT.scss']
   },
   output: {
     path: path.resolve(__dirname, './dist'), // 뱉어낼 파일 경로
-    publicPath: 'dist/', // 파일들이 위치할 서버상의 경로
-    filename: 'app.js',
-    hotUpdateChunkFilename: 'hot/hot-update.js', // 내가 지금까지 발견 한 가장 좋은 방법은 해당 청크에 대한 특정 폴더와 파일을 정의하는 것
-    hotUpdateMainFilename: 'hot/hot-update.json' // 내가 지금까지 발견 한 가장 좋은 방법은 해당 청크에 대한 특정 폴더와 파일을 정의하는 것
-    // chunkFilename: "[id].js"
+    publicPath: '/', // 파일들이 위치할 서버상의 경로
+    filename: '[name].js',
+    clean: true
   },
   module: {
     rules: [
       {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-        /*
-          options: {
-              loaders: {
-                  css: ExtractTextPlugin.extract({
-                      use: 'css-loader',
-                      fallback: 'vue-style-loader'
-                  })
+        test: /\.js$|jsx/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            '@babel/preset-env', 
+            ['@babel/preset-react', {"runtime": "automatic"}]
+          ],
+        },
+      },     
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+              sourceMap: true,
+              url: false,
+            }
+          },
+          /* {
+            loader: 'resolve-url-loader'
+          }, */
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  "autoprefixer",
+                ]
               }
-          }
-        } */
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
+            }
+          }, 
           {
-            loader: 'babel-loader',
+            loader: 'sass-loader',
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+              sourceMap: true,  // <-- IMPORTANT!
             }
           }
-        ]
+        ],
       },
-      {
-        test: /\.css$/,
-        use: [
-          // process.env.NODE_ENV !== 'production' ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {importLoaders: 1}
-          }
-          // 'postcss-loader'
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'less-loader']
-        })
-      },
-      {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/dist/img/',
-              name: 'img/[name].[hash:10].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(svg)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            name: '[name].[ext]?[hash]',
-            publicPath: '/dist/',
-            limit: 10000 // 10kb
-          }
-        }
-      }
     ]
   },
   plugins: [
-    new VueLoaderPlugin(),
-    new webpack.LoaderOptionsPlugin({
-      minimize: false
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "[id].css"
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new LiveReloadPlugin({
-      protocol: 'http',
-      port: 35729,
-      hostname: 'localhost',
-      // hostname: '106.10.58.121/',
-      appendScriptTag: true
-    }),
-    new webpack.NamedModulesPlugin(), // 브라우저에서 HMR 에러발생시 module name 표시
     new HtmlWebpackPlugin({
-      template: 'index.html',
-      inject: false, // index 주입 false
+      template: path.resolve(process.cwd(), "./index.html"),
+      filename: "index.html",
+      inject: true, // index 주입 false
       hash: true,
-      chunks: ['index', 'app', 'system', 'monitor'],
       minify: { // (https://github.com/kangax/html-minifier)
         collapseWhitespace: false, // 문서 트리의 텍스트 노드에 공헌하는 공백 축소
         removeAttributeQuotes: false, // 따옴표
         removeComments: false // 주석
       }
     }),
-    new ExtractTextPlugin('css/AdminLTE.css')
+    /* 
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: 'dist'}
+      ],
+    }),
+     */
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
+    })
   ],
   resolve: {
-    modules: [
-      path.resolve(__dirname, 'node_modules')
-    ],
-    extensions: ['.js', '.json', '.jsx', '.css', '.less'],
-    alias: { vue: 'vue/dist/vue.esm.js' }
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.js', '.jsx','.scss','.html'],
   },
   optimization: {
-    minimize: false,
-    splitChunks: {},
-    concatenateModules: true
+    runtimeChunk: 'single'
   },
   externals: {
     // global app config object
     config: JSON.stringify({
-      apiUrl: 'http://10.41.76.139:2294'
-      // apiUrl: 'http://10.41.76.139:22'
+        apiUrl: 'http://localhost:4000'
     })
-  }
-};
-
+  },
+}
